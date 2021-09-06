6fd49538e35110d4c53903b06730e3772bf4ba89
SONGYOUNGWOO <jeroso@ldfactory.com>
2021-09-06 11:23:54 +0900

    commit

---
+++ src/BOJ/_10799_쇠막대기/Main.java
@@ -0,0 +1,29 @@
+package BOJ._10799_쇠막대기;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine().trim();
+        int n = a.length();
+        Stack<Integer> s = new Stack<Integer>();
+        int ans = 0;
+        for (int i=0; i<n; i++) {
+            char c = a.charAt(i);
+            if (c == '(') {
+                s.push(i);
+            } else {
+                if (s.peek()+1 == i) {
+                    s.pop();
+                    ans += s.size();
+                } else {
+                    s.pop();
+                    ans += 1;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
