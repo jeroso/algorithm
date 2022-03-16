0dc6eba2071682dbe96527ba953de6ebfaa6db02
jeroso <jeroso@nate.com>
2022-03-16 23:55:19 +0900

    패스트 캠퍼스 알고리즘 강좌

---
+++ src/fastCampus/Chapter1/Chapter1Section1.java
@@ -0,0 +1,23 @@
+package fastCampus.Chapter1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Chapter1Section1 {
+    static StringBuilder sb = new StringBuilder();
+
+    static void input(){
+
+    }
+    static int N, M;
+    static int[] selected;
+
+    public static void rect_func(int k) {
+
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
