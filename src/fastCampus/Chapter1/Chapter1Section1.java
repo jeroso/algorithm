745b2c879b2ad16c886b7fbbaeded1ed1515ea07
jeroso <jeroso@nate.com>
2022-04-01 23:54:43 +0900

    commit

--- src/fastCampus/Chapter1/Chapter1Section1.java
+++ src/fastCampus/Chapter1/Chapter1Section1.java
@@ -1,14 +1,18 @@
 package fastCampus.Chapter1;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
+//15651 N과 M (3)
 public class Chapter1Section1 {
     static StringBuilder sb = new StringBuilder();
 
     static void input(){
-
+        FastReader scan = new FastReader();
+        N = scan.next
     }
     static int N, M;
     static int[] selected;
@@ -17,6 +21,41 @@ public class Chapter1Section1 {
 
     }
     public static void main(String[] args) {
+
+
+    }
+
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        Double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
 
     }
