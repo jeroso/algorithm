ff9295ebec211d2a242e68aeca34e88b3952929d
woodsy <jeroso4481@gmail.com>
2022-04-12 23:40:31 +0900

    commit

--- src/fastCampus/나동빈/Chapter1Section1.java
+++ src/fastCampus/나동빈/Chapter1Section1.java
@@ -7,9 +7,6 @@ import java.util.StringTokenizer;
 public class Chapter1Section1 {
 
 
-<<<<<<< HEAD
-=======
-    }
     static StringBuilder sb = new StringBuilder();
 
 
@@ -20,16 +17,5 @@ public class Chapter1Section1 {
     static void rec_func(){
 
     }
-
-    //전구 개수
-    static int N;
-    //입력되는 명령어 개수
-    static int M;
-
-<<<<<<< HEAD
-    
-=======
->>>>>>> 2c7d9dbab4267a07e4e910be65de64cde48339eb
->>>>>>> 136376fb04610787ad25427c1d443f99e6cd029d
 
 }
--- src/programmers/Hash/위장/Solution.java
+++ src/programmers/Hash/위장/Solution.java
@@ -1,12 +1,23 @@
 package programmers.Hash.위장;
 
+import java.util.HashMap;
+
 public class Solution {
 
-    public int soulution(String[][] clothes) {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
 
-        return 0;
+        for (String[] clothe:
+             clothes) {
+            String type = clothe[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        int answer = 0;
+        return answer;
     }
     public static void main(String[] args) {
-
+        String[][] arr = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        Solution sol = new Solution();
+        sol.solution(arr);
     }
 }
