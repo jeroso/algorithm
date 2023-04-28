5d8d9a6fe09311679b93f5ed2f04f12d06b3ba8a
youngwoo-song <yw.song@fiet.net>
2023-04-28 18:39:54 +0900

    후위연산식

---
+++ src/Inflearn/Section05/후위식연산/Main.java
@@ -0,0 +1,38 @@
+package Inflearn.Section05.후위식연산;
+
+import BOJ_basic.FastReader;
+import jdk.dynalink.beans.StaticClass;
+
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public int Solution(String text) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (char c : text.toCharArray()) {
+            if (Character.isDigit(c)) {
+                stack.push(c - 48); //입력된 정수값으로 stack push
+            }else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if(c == '+') stack.push(lt + rt);
+                else if (c == '-') stack.push(lt - rt);
+                else if (c == '*') stack.push(lt * rt);
+                else if (c == '/') stack.push(lt / rt);
+            }
+            answer = stack.get(0);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FastReader scan = new FastReader();
+
+        String next = scan.next();
+        Main main = new Main();
+        main.Solution(next);
+    }
+}
