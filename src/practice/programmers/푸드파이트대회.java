f049aa8535488aa2dc23a5258ad33571a2ea9920
youngwoo-song <yw.song@fiet.net>
2025-04-08 10:52:40 +0900

    programmers

---
+++ src/practice/programmers/푸드파이트대회.java
@@ -0,0 +1,40 @@
+package practice.programmers;
+
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/134240
+public class 푸드파이트대회 {
+    // 내가 푼 방식
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                sb.append(i);
+            }
+        }
+        answer += sb;
+        answer += 0;
+        answer += sb.reverse();
+        return answer;
+    }
+    // 다른 사람이 푼 방식
+    public String solution2(int[] food) {
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        푸드파이트대회 main = new 푸드파이트대회();
+        String case1 = main.solution(new int[]{1, 3, 4, 6});
+        String case2 = main.solution(new int[]{1, 7, 1, 2});
+        System.out.println(case1);
+        System.out.println(case2);
+    }
+}
---
+++ src/practice/programmers/햄버거만들기.java
@@ -0,0 +1,38 @@
+package practice.programmers;
+
+import java.util.*;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/133502
+public class 햄버거만들기 {
+
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        //1231 순서
+        List<Integer> list = new LinkedList<>();
+        for (int i : ingredient) {
+            list.add(i);
+            if (list.size() >= 4) {
+                if (list.get(list.size() - 4) == 1
+                        && list.get(list.size() - 3) == 2
+                        && list.get(list.size() - 2) == 3
+                        && list.get(list.size() - 1) == 1) {
+                    answer++;
+                    list.remove(list.size() - 4);
+                    list.remove(list.size() - 3);
+                    list.remove(list.size() - 2);
+                    list.remove(list.size() - 1);
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        햄버거만들기 main = new 햄버거만들기();
+        int case1 = main.solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1});
+        int case2 = main.solution(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2});
+        System.out.println(case1);
+        System.out.println(case2);
+    }
+}
